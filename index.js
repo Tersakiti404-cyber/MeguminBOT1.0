51e685d551e921f91285f021535c147750570a74
Tersakiti404-cyber <76648497+Tersakiti404-cyber@users.noreply.github.com>
2021-02-01 15:18:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -422,8 +422,8 @@ async function starts() {
                         pushname = client.contacts[sender] != undefined ? client.contacts[sender].vname || client.contacts[sender].notify : undefined
 
                         // Options
-                        const botName = 'BOT_NAME'
-                        const ownerName = 'OWNER_NAME'
+                        const botName = '𝙼𝚎𝚐𝚞𝚖𝚒𝚗 𝙱𝙾𝚃'
+                        const ownerName = 'Fajar Alfarizi'
                         const BarBarKey = 'YOUR_APIKEY'
 
 			const isUrl = (url) => {
